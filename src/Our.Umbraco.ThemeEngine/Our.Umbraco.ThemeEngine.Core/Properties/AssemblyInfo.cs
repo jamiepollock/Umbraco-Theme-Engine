faae8ec6d674bb7d816b4783812c8da07fc9868d
jamiepollock <jamie.does.umbraco@gmail.com>
2016-12-20 16:52:19 -0500

    Updated properties in preperation for build process

--- src/Our.Umbraco.ThemeEngine/Our.Umbraco.ThemeEngine.Core/Properties/AssemblyInfo.cs
+++ src/Our.Umbraco.ThemeEngine/Our.Umbraco.ThemeEngine.Core/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Our.Umbraco.ThemeEngine.Core")]
+[assembly: AssemblyTitle("Our.Umbraco.MigrationViewer.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Our.Umbraco.ThemeEngine.Core")]
+[assembly: AssemblyProduct("Our.Umbraco.MigrationViewer.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,18 +19,5 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("61fac6a1-b0c2-44bf-a921-17061ffa31fc")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM 
+[assembly: Guid("50708649-5de5-4fff-9396-4fc2744ea0ba")] 
--- src/Our.Umbraco.ThemeEngine/Our.Umbraco.ThemeEngine.Web/Properties/AssemblyInfo.cs
+++ src/Our.Umbraco.ThemeEngine/Our.Umbraco.ThemeEngine.Web/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Our.Umbraco.ThemeEngine.Web")]
+[assembly: AssemblyTitle("Our.Umbraco.MigrationViewer.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Our.Umbraco.ThemeEngine.Web")]
+[assembly: AssemblyProduct("Our.Umbraco.MigrationViewer.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,18 +19,5 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4d46dc6d-cca5-48fd-a2fd-ce4297bd4d3f")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM 
+[assembly: Guid("50708649-5de5-4fff-9396-4fc2744ea0ba")] 
