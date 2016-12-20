c3c34e804335f102da90e39e109eb631b37dbff7
jamiepollock <jamie.does.umbraco@gmail.com>
2016-12-20 13:44:41 -0500

    Added Web project with property editor and empty themes folder

---
+++ src/Our.Umbraco.ThemeEngine/Our.Umbraco.ThemeEngine.Web/App_Plugins/ThemeEngine/PropertyEditors/themepicker.controller.js
@@ -0,0 +1,12 @@
+﻿angular.module("umbraco")
+    .controller("Our.Umbraco.ThemeEngine.ThemePickerController",
+    function ($scope, umbRequestHelper, $http) {
+        var url = "api/ThemeEnginePropertyEditorApi/GetThemes";
+
+        umbRequestHelper.resourcePromise(
+                $http.get(url),
+                'Failed to retrieve themes for Site at ' + url)
+            .then(function (data) {
+                $scope.themes = data;
+            });
+    });
