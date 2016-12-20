305e62afed5fa5fdd8a37ea53c69835bfc1de37c
jamiepollock <jamie.does.umbraco@gmail.com>
2016-12-20 13:34:25 -0500

    Added API Controller

---
+++ src/Our.Umbraco.ThemeEngine/Our.Umbraco.ThemeEngine.Core/Controllers/ThemeEnginePropertyEditorApiController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web.Http;
+
+using Umbraco.Core.IO;
+using Umbraco.Web.WebApi;
+
+namespace Our.Umbraco.ThemeEngine.Core.Controllers
+{
+    public class ThemeEnginePropertyEditorApiController : UmbracoApiController
+    {
+        //  /Umbraco/Api/ThemeEnginePropertyEditorApi/GetThemes
+        [HttpGet]
+        public IEnumerable<string> GetThemes()
+        {
+            var dir = IOHelper.MapPath("~/App_Plugins/ThemeEngine/Themes");
+            var allDirs = Directory.GetDirectories(dir).Select(x => new DirectoryInfo(x).Name);
+            return allDirs;
+        }
+    }
+}
