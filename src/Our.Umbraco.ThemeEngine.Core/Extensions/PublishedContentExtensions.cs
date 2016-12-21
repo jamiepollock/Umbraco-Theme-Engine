18b8eca3f9f1580d529bc291be0af3fbe6bee586
jamiepollock <jamie.does.umbraco@gmail.com>
2016-12-21 00:24:06 -0500

    Added GetTheme() extension method for IPublishedContent (see desc)
    This is to allow for reusable settings to be configured at a config level without having to request the same details over and over.
    
    At some time there should be some overloads or a user could simply use the Umbraco Core function, GetPropertyValue<string>() for more granular control.

---
+++ src/Our.Umbraco.ThemeEngine.Core/Extensions/PublishedContentExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Our.Umbraco.ThemeEngine.Core.Configuration;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+
+namespace Our.Umbraco.ThemeEngine.Core.Extensions
+{
+    public static class PublishedContentExtensions
+    {
+        private static IThemeEngineConfiguration _configuration
+        {
+            get
+            {
+                return ThemeEngineConfigurationSection.GetConfiguration();
+            }
+        } 
+
+        public static string GetTheme(this IPublishedContent content)
+        {
+            var hasDefaultValue = string.IsNullOrWhiteSpace(_configuration.DefaultTheme) == false;
+
+            return hasDefaultValue ? content.GetPropertyValue(_configuration.ThemePropertyAlias, _configuration.FindThemePropertyRecursively, _configuration.DefaultTheme)
+                                   : content.GetPropertyValue<string>(_configuration.ThemePropertyAlias, _configuration.FindThemePropertyRecursively);
+        }
+    }
+}
