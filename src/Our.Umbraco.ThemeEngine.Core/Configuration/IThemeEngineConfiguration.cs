c8e602af7ffec50efa39474effca40b429587363
jamiepollock <jamie.does.umbraco@gmail.com>
2016-12-21 00:21:29 -0500

    Added configuration files

---
+++ src/Our.Umbraco.ThemeEngine.Core/Configuration/IThemeEngineConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace Our.Umbraco.ThemeEngine.Core.Configuration
+{
+    public interface IThemeEngineConfiguration
+    {
+        string DefaultTheme { get; set; }
+        bool FindThemePropertyRecursively { get; set; }
+        string ThemePropertyAlias { get; set; }
+    }
+}
---
+++ src/Our.Umbraco.ThemeEngine.Core/Configuration/ThemeEngineConfigurationSection.cs
@@ -0,0 +1,60 @@
+﻿using System.Configuration;
+
+namespace Our.Umbraco.ThemeEngine.Core.Configuration
+{
+    public class ThemeEngineConfigurationSection : ConfigurationSection, IThemeEngineConfiguration
+    {
+        private const string ThemeEngineConfigurationSectionName = "Our.Umbraco.ThemeEngine";
+
+        private const string DefaultThemeConfigurationPropertyName = "defaultTheme";
+        private const string FindThemePropertyRecursivelyPropertyName = "findThemePropertyRecursively";
+        private const string ThemePropertyAliasConfigurationPropertyName = "themePropertyAlias";
+
+        [ConfigurationProperty(name: DefaultThemeConfigurationPropertyName, IsRequired = false)]
+        public string DefaultTheme
+        {
+            get
+            {
+                return base[DefaultThemeConfigurationPropertyName] as string;
+            }
+
+            set
+            {
+                base[DefaultThemeConfigurationPropertyName] = value;
+            }
+        }
+
+        [ConfigurationProperty(name: FindThemePropertyRecursivelyPropertyName, IsRequired = true, DefaultValue = true)]
+        public bool FindThemePropertyRecursively
+        {
+            get
+            {
+                return (bool)base[FindThemePropertyRecursivelyPropertyName];
+            }
+
+            set
+            {
+                base[FindThemePropertyRecursivelyPropertyName] = value;
+            }
+        }
+
+        [ConfigurationProperty(name: ThemePropertyAliasConfigurationPropertyName, IsRequired = true)]
+        public string ThemePropertyAlias
+        {
+            get
+            {
+                return base[ThemePropertyAliasConfigurationPropertyName] as string;
+            }
+
+            set
+            {
+                base[ThemePropertyAliasConfigurationPropertyName] = value;
+            }
+        }
+
+        public static IThemeEngineConfiguration GetConfiguration()
+        {
+            return ConfigurationManager.GetSection(ThemeEngineConfigurationSectionName) as ThemeEngineConfigurationSection;
+        }
+    }
+}
